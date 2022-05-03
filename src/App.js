b58ec0150176deabf70442b6b075a61cc60c41f6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-04 02:00:45 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import UserList from './pages/userList/UserList';
 
+import "./App.css";
+import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router className="App">
+    
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+        <Route path="/users" element={<UserList/>} />
+        <Route exact path="/user/:userId" element={<User />} />
+        <Route exact path="/newUser" element={<NewUser />} />
+        <Route exact path="/products" element={<ProductList />} />
+        <Route exact path="/productEdit/:userId" element={<Product />} />
+        <Route exact path="/newproduct" element={<NewProduct />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,117 @@
+export const userAndSalesData = [
+    {
+      name: "Jan",
+      sales: 4000,
+      users: 2400,
+     
+    },
+    {
+      name: "Feb",
+      sales: 3000,
+      users: 1398,
+    
+    },
+    {
+      name: "March",
+      sales: 2000,
+      users: 9800,
+      
+    },
+    {
+      name: "April",
+      sales: 2780,
+      users: 3908,
+    
+    },
+    {
+      name: "May",
+      sales: 1890,
+      users: 4800,
+    
+    },
+    {
+      name: "June",
+      sales: 2390,
+      users: 3800,
+      
+    },
+    {
+      name: "July",
+      sales: 3490,
+      users: 4300,
+    
+    },
+    {
+      name: "Aug",
+      sales: 4000,
+      users: 2400,
+  
+    },
+    {
+      name: "Sept",
+      sales: 3000,
+      users: 1398,
+    
+    },
+    {
+      name: "Oct",
+      sales: 2000,
+      users: 9800,
+  
+    },
+    {
+      name: "Nov",
+      sales: 2780,
+      users: 3908,
+  
+    },
+    {
+      name: "Dec",
+      sales: 1890,
+      users: 4850,
+    },
+  
+  ];
+
+  export const userRows = [
+    { id: 1, username: 'Jon Snow',avatar:"https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341", email: 'Jon@gmail.com', status: 'Active', transaction: 'Rs.1,000' },
+    { id: 2, username: 'Ceriee Lannister',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Cersei@gmail.com', status: 'Active', transaction: 'Rs.2,000' },
+    { id: 3, username: 'Jaime Lannister',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Jaime@gmail.com', status: 'Active' , transaction: 'Rs.3,000' },
+    { id: 4, username: 'Arya Stark',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Arya@gmail.com', status: 'Active' , transaction: 'Rs.4,000' },
+    { id: 5, username: 'Daenerys Targaryen',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Daenerys@gmail.com', status: 'Active' , transaction: 'Rs.5,000' },
+    { id: 6, username: 'Melisandre',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Melisandre@gmail.com', status: 'Active' , transaction: 'Rs.6,000' },
+    { id: 7, username: 'Ferrara Clifford',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Ferrara@gmail.com', status: 'Active' , transaction: 'Rs.7,000' },
+    { id: 8, username: 'Rossini Frances',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Rossini@gmail.com', status: 'Active', transaction: 'Rs.8,000' },
+    { id: 9, username: 'Harvey Roxie',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Harvey@gmail.com', status: 'Active', transaction: 'Rs.9,000' },
+    { id: 10, username: 'Harvey Roxie',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Harvey@gmail.com', status: 'Active', transaction: 'Rs.9,000' },
+  ];
+
+
+  export const productRows = [
+    { id: 1, name: 'Iphone',img:"https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", stock: '120', status: 'Active', price: 'Rs.1,000' },
+    { id: 2, name: 'Earpods',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '100', status: 'Active', price: 'Rs.2,000' },
+    { id: 3, name: 'Mac Book',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '10', status: 'Active' , price: 'Rs.3,000' },
+    { id: 4, name: 'Infinix Hot 8',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '50', status: 'Active' , price: 'Rs.4,000' },
+    { id: 5, name: 'C-type Charger',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '0', status: 'Active' , price: 'Rs.5,000' },
+    { id: 6, name: 'Audionic Speaker',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '17', status: 'Active' , price: 'Rs.6,000' },
+    { id: 7, name: 'Laptop Bag',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '39', status: 'Active' , price: 'Rs.7,000' },
+    { id: 8, name: 'Core i5 PC',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '99', status: 'Active', price: 'Rs.8,000' },
+    { id: 9, name: '8GB USB',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '54', status: 'Active', price: 'Rs.9,000' },
+    { id: 10, name: 'Smart Watch',img:'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', stock: '0', status: 'Active', price: 'Rs.9,000' },
+  ];
+
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
