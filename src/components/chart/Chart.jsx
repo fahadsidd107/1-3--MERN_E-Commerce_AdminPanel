6b65f1802f8164f16ad64032392c2231bda402cc
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-12 04:01:10 +0500

    work complete

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import './Chart.css'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from "react";
+import "./Chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-
-function Chart({ title, data, userDataKey,salesDataKey, grid }) {
+function Chart({ title, data, userDataKey, salesDataKey, grid }) {
   return (
-    <div className='chart'>
-    <h3 className='chartTitle'>{title}</h3>
-    <ResponsiveContainer width='100%' aspect={4 / 1}>
-<LineChart data={data}>
-<XAxis dataKey="name" stroke='#5551bd' />
-<YAxis /> 
-<Line type="monotone" dataKey={salesDataKey} stroke="#5551bd" />
-<Line type="monotone" dataKey={userDataKey} stroke="rgb(16, 170, 124)" />
-<Legend />
-<Tooltip/>
-{grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
-</LineChart>
-    </ResponsiveContainer>
+    <div className="chart">
+      <h3 className="chartTitle">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5551bd" />
+          <YAxis />
+          <Line type="monotone" dataKey={salesDataKey} stroke="#5551bd" />
+          <Line
+            type="monotone"
+            dataKey={userDataKey}
+            stroke="rgb(16, 170, 124)"
+          />
+          <Legend />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
     </div>
-  )
+  );
 }
 
-export default Chart
+export default Chart;
