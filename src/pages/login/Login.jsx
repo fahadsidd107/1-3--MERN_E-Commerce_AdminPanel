f2840ea9325d7b443f7450b1fd504d109a7a77e6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-01 00:20:36 +0500

    a

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function Login() {
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
   return (
     <div
     style={{
