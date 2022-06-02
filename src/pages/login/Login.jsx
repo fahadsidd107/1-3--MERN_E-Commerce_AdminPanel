595151db9018b7eff46910a4deb7d6b0217099f0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-02 20:15:46 +0500

    work complete

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleClick=(e)=>{
-        e.preventDefault();
-    }
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
   return (
     <div
-    style={{
-      height: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
-  >
-        <input type='email' placeholder='email' onChange={(e) => setEmail(e.target.value)} />
-        <input type='password' placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
-        <button onClick={handleClick}>Login</button>
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Login</button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
