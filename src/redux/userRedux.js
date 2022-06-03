58cefe2eb3de1326c9b21d31f125dc3de17e937e
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-03 17:49:57 +0500

    work complete

--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -3,15 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-currentUser: null,
-isFetching: false,
-error: false,
+    currentUser: null,
+    isFetching: false,
+    error: false,
   },
   reducers: {
     loginStart: (state) => {
       state.isFetching = true;
     },
-    loginSuccess: (state,action) => {
+    loginSuccess: (state, action) => {
       state.isFetching = true;
       state.currentUser = action.payload;
     },
@@ -22,5 +22,5 @@ error: false,
   },
 });
 
-export const { loginStart,loginSuccess,loginFailure  } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
 export default userSlice.reducer;
