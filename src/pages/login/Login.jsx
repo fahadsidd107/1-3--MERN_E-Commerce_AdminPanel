7142cf2883c03ec0aeddff71e527e9e3fcf4ef29
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-05 22:05:48 +0500

    work complete

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import useDispatch from "./redux/useDispatch";
+import useDispatch from "react-redux";
 
 function Login() {
   const [email, setEmail] = useState("");
