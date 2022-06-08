69260c906e135643962370858ddb9d6fb992644a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-08 19:48:49 +0500

    work complete

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:7777/api/"
 const TOKEN = ""
 
-console.log()
+console.log(localStorage.getItem("persist:root"))
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
