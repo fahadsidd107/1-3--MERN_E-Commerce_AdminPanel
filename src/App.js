c257d053aa0fd8fd45618bd9a447b4c36fcee705
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-09 18:09:46 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   const admin=true
   return (
     <Router>
-    <Topbar/>
+    {<Topbar/>
      <div className="container">
       <Sidebar />
       <Routes>
@@ -35,7 +35,7 @@ function App() {
             <Route path="/product/:productId" element={<Product />}/>
             <Route path="/newproduct" element={<NewProduct />}/>
     </Routes>
-      </div>
+      </div>}
  
   </Router>
 
