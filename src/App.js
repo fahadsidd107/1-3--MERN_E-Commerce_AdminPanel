af838997357f6e2938d6b51657b70565099a3eb6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-10 02:21:40 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
@@ -19,32 +19,30 @@ import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 function App() {
-  const admin= JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
+  const admin = JSON.parse(
+    JSON.parse(localStorage.getItem("persist:root")).user
+  ).currentUser.isAdmin;
   return (
     <Router>
-    {admin && (
-      <>
-    <Topbar/>
-     <div className="container">
-      <Sidebar />
-      <Routes>
-      <Route path="/login" element={<Login />}/>
-            <Route exact path="/" element={<Home />}/>
-            <Route path="/users" element={<UserList />}/>
-            <Route path="/user/:userId" element={<User />}/>
-            <Route path="/newUser" element={<NewUser />}/>
-            <Route path="/products" element={<ProductList />}/>
-            <Route path="/product/:productId" element={<Product />}/>
-            <Route path="/newproduct" element={<NewProduct />}/>
-    </Routes>
-      </div>
-      </>
-    )
-      
-      }
- 
-  </Router>
-
+      {admin && (
+        <>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route exact path="/" element={<Home />} />
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/newproduct" element={<NewProduct />} />
+            </Routes>
+          </div>
+        </>
+      )}
+    </Router>
   );
 }
 
