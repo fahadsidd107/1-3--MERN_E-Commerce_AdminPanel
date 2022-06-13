b1d5c1fa9aff01522d3d5dd00f4564ee64313551
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-14 00:27:21 +0500

    work complete

--- src/components/widgetsm/WidgetSm.jsx
+++ src/components/widgetsm/WidgetSm.jsx
@@ -1,54 +1,45 @@
-import React, { useState, useEffect } from "react";
 import "./WidgetSm.css";
 import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
 import { userRequest } from "../../requestMethods";
-function WidgetSm({ data, uname, utitle }) {
+
+export default function WidgetSm() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const res = await userRequest.get("user/?new=true");
+        const res = await userRequest.get("users/?new=true");
         setUsers(res.data);
-      } catch (err) {
-        console.log(err);
-      }
+      } catch {}
     };
     getUsers();
   }, []);
+  
   return (
-    <div className="WidgetSm">
-      <span className="widgetSmTitle">New Join Memebers</span>
-      <ul className="WidgetSmList">
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
         {users.map((user) => (
-          <ListItem
-            name={user.username}
-            image={
-              user.image ||
-              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-            }
-          />
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              src={
+                user.img ||
+                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
-
-export function ListItem({ name, image }) {
-  return (
-    <div>
-      <li className="WidgetSmListItem">
-        <img src={image} alt="user" className="widgetSmImg" />
-        <div className="widgetSmUser">
-          <span className="widgetUserName">{name}</span>
-        </div>
-        <button className="widgetSmButton">
-          <Visibility className="widgetSmIcon" />
-          Display
-        </button>
-      </li>
-    </div>
-  );
-}
-
-export default WidgetSm;
