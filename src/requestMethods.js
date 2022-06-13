075c683604ff5e179be46305c808995106b10482
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-14 00:25:43 +0500

    work complete

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:7777/api/"
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
 
-console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
+
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
@@ -11,5 +11,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header: {token:`Bearer ${TOKEN}`}
+    headers: {token:`Bearer ${TOKEN}`}
 })
