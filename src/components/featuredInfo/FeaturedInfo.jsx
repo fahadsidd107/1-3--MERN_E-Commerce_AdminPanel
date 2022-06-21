b545d9bafb887c9a8417fbd0126cec0d597cb876
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-21 20:25:14 +0500

    work complete

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -12,7 +12,7 @@ function FeaturedInfo() {
       try{
         const res = await userRequest(`/order/income`);
         setIncome(res.data);
-        setPercentage((res.data[1].total*100)/(2));
+        setPercentage((res.data[1].total*100)/(res.data[0].total));
       }catch(err){
         console.log(err)
       }
