c402ed61c8b0a3a0a0c58369c36d24a7ba606362
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-23 17:50:27 +0500

    work complete

--- src/redux/productRedux.js
+++ src/redux/productRedux.js
@@ -1,1 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+export const {
+    getProductStart,
+    getProductSuccess,
+    getProductFailure,
+    deleteProductStart,
+    deleteProductSuccess,
+    deleteProductFailure,
+    updateProductStart,
+    updateProductSuccess,
+    updateProductFailure,
+    addProductStart,
+    addProductSuccess,
+    addProductFailure,
+  } = productSlice.actions;
