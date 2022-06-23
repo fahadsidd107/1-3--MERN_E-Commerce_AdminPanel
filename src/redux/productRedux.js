ebb3133c406cad9a20040faf51b32df30906b73b
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-24 03:55:00 +0500

    work complete

--- src/redux/productRedux.js
+++ src/redux/productRedux.js
@@ -1,40 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const productSlice = createSlice({
-    name: "product",
+  name: "product",
   initialState: {
     products: [],
     isFetching: false,
     error: false,
   },
   reducers: {
-        //GET ALL
-        getProductStart: (state) => {
-            state.isFetching = true;
-            state.error = false;
-          },
-          getProductSuccess: (state, action) => {
-            state.isFetching = false;
-            state.products = action.payload;
-          },
-          getProductFailure: (state) => {
-            state.isFetching = false;
-            state.error = true;
-          },
-          //DELETE
+    //GET ALL
+    getProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    getProductSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products = action.payload;
+    },
+    getProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    //DELETE
     deleteProductStart: (state) => {
-        state.isFetching = true;
-        state.error = false;
-      },
-  }
+      state.isFetching = true;
+      state.error = false;
+    },
+  },
+});
 
-})
+export const { getProductStart, getProductSuccess, getProductFailure } =
+  productSlice.actions;
 
-export const {
-    getProductStart,
-    getProductSuccess,
-    getProductFailure,
-  } = productSlice.actions;
-  
-  export default productSlice.reducer;
-
+export default productSlice.reducer;
