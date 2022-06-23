7703cea0f1f1413e6dfb859f2227aff09a913d6c
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-24 03:47:47 +0500

    work

--- src/redux/productRedux.js
+++ src/redux/productRedux.js
@@ -7,12 +7,10 @@ export const productSlice = createSlice({
     isFetching: false,
     error: false,
   },
-  reducers: {}
-    //GET ALL
-    getProductStart: (state) => {
-        state.isFetching = true;
-        state.error = false;
-      },
+  reducers: {
+    
+  }
+
 })
 
 
