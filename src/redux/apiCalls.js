4c46118017ce9f9e650fcb16133d299ee3b31b73
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-25 03:34:23 +0500

    work complete

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,5 +1,5 @@
 import { loginStart,loginFailure,loginSuccess } from "./userRedux";
-import {getProductStart} from './productRedux'
+import {getProductStart, getProductSuccess} from './productRedux'
 import { userRequest } from "../requestMethods";
 
 export const login = async (dispatch,user)=>{
@@ -16,9 +16,9 @@ export const login = async (dispatch,user)=>{
 export const getProduct = async (dispatch)=>{
     dispatch(getProductStart());
     try{
-        const res = await userRequest.post("/auth/login",user);
-        dispatch(loginSuccess(res.data));
+        const res = await userRequest.post("/product");
+        dispatch(getProductSuccess(res.data));
     }catch(err){
-        dispatch(loginFailure());
+        dispatch(());
     }
 }
