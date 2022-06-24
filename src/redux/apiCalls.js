744d56ccb3b1f5c06dc8d277f82caf648b317f33
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-25 03:30:21 +0500

    work

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -10,3 +10,14 @@ export const login = async (dispatch,user)=>{
         dispatch(loginFailure());
     }
 }
+
+
+export const login = async (dispatch,user)=>{
+    dispatch(loginStart());
+    try{
+        const res = await userRequest.post("/auth/login",user);
+        dispatch(loginSuccess(res.data));
+    }catch(err){
+        dispatch(loginFailure());
+    }
+}
