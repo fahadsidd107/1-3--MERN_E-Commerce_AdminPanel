4361055b08fbd44ba7ca155b2bfeb1abbff6e038
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-25 03:36:29 +0500

    work complete

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -3,6 +3,7 @@ import { DataGrid } from "@mui/x-data-grid";
 import { Edit, DeleteOutline } from "@material-ui/icons";
 import { productRows } from "../../Data";
 import { Link } from "react-router-dom";
+import {useDispatch} from 'react-redux'
 import "./ProductList.css";
 function ProductList() {
   const [data, setData] = useState(productRows);
