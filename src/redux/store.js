cbf02d82ca5b185254fe68a4655906a87ce0a9b0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-27 19:07:50 +0500

    work

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import userReducer from "./userRedux";
+import productReducer from "./productRedux";
 import {
   persistStore,
   persistReducer,
