3c8e9b638de12ac706874bf66f2df82bcdea02f3
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-29 18:31:54 +0500

    work complete

--- src/redux/productRedux.js
+++ src/redux/productRedux.js
@@ -26,13 +26,7 @@ export const productSlice = createSlice({
       state.isFetching = true;
       state.error = false;
     },
-    deleteProductSuccess: (state, action) => {
-      state.isFetching = false;
-      state.products.splice(
-        state.products.findIndex((item) => item._id === action.payload),
-        1
-      );
-    },
+
   },
 });
 
